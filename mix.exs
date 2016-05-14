e21c0e39ad05928dcd1b71a310e4e507a541173f
Timur Yanberdin <yanberdint@gmail.com>
2016-05-14 12:56:59 +0300

    version 0.0.2

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule SplatoonMapsBot.Mixfile do
 
   def project do
     [app: :splatoon_maps_bot,
-     version: "0.0.1",
+     version: "0.0.2",
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
